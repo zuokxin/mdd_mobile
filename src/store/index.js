5ea9cb979c50f8913204efae57eaf68237cb6dc1
陆兵华 <lubinghua@fanhantech.com>
2022-05-07 10:08:11 +0000

    fix （md-mobile） 验证手机号

--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,8 @@ export default new Vuex.Store({
   },
   getters: {
     isLogin: () => (phone) => {
-      const reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
-      return reg.test(phone)
+      // const reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+      return String(phone).length === 11
     }
   },
   mutations: {
