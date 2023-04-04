cec4f11dcba8a1739ee5463f8141ba60a430aadc
linmiaomiao <linmiaomiao@fanhantech.com>
2023-04-04 02:33:26 +0000

    feat:虚拟人和二维码

--- src/views/test-do/test-do-fadi/dialog-box-left.vue
+++ src/views/test-do/test-do-fadi/dialog-box-left.vue
@@ -1,23 +1,35 @@
 <template>
-  <div>
+  <div class="dialog-box-left">
+    <!-- <video
+      v-if="!isHistory"
+      ref="curVideo"
+      class="dialog-box-left-video"
+      :src="url"
+      autoplay
+      @play="played"
+      @pause="paused"
+    ></video> -->
     <div class="main">
       <div class="boxLeft" :style="{color: '#333' }">{{textLeft}}</div>
-      <div class="laba" :style="{background: !isPlay || isHistory ? '#fff':'#34B7B9' }" @click.once="playAudio">
+      <div v-if="isReady" class="laba" :style="{background: !isPlay || isHistory ? '#fff':'#34B7B9' }" @click.once="pauseVideo">
         <img v-show="isPlay  && !isHistory" src="@/assets/img/test/horn.gif" width="12" height="12" alt="-">
         <img v-if="(!isPlay && !isHistory) || isHistory" src="@/assets/img/test/speaker.png" alt="speaker" style="width: 13px; height: 13px;">
+      </div>
+      <div v-else class="laba run" :style="{background: '#fff' }">
+        <icon-font name="#mdd-loading-01" color="#999"></icon-font>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Music from '@/utils/music'
+import { Dialog } from 'vant'
 export default {
   name: 'dialog-box-left',
   props: {
     textLeft: String,
     url: String,
-    needAnswer: Boolean,
+    // 历史记录显示
     isHistory: {
       type: Boolean,
       default: false
@@ -26,59 +38,130 @@ export default {
   data () {
     return {
       isPlay: true, // 正在播放
-      player: null
+      isReady: false, // 是否正在加载
+      timer: null,
+      startTime: 0,
+      videoEle: null
     }
   },
   mounted () {
-    if (this.url) {
-      const vm = this
-      this.player = new Music(this.url)
-      this.player.addEventListener('load', function () {
-        this.play()
-        const interval = setInterval(() => {
-          if (this.source.playStatus !== 2) {
-            clearInterval(interval)
-            vm.isPlay = false
-            vm.player = null
-            vm.$emit('palyAudio', true)
-          }
-        }, 0)
+    this.$nextTick(() => {
+      // 历史不读
+      if (this.isHistory) return
+      // 创建视频元素，每一个语音创建一个视频保证视频衔接的流畅
+      this.videoEle = document.getElementById('dialogVideo')
+      this.videoEle = document.createElement('video')
+      this.videoEle.className = 'dialog-box-left-video'
+      this.videoEle.id = 'dialogVideo'
+      this.videoEle.src = this.url
+      this.videoEle.autoplay = true
+      // 插入fadi页面中，后退可一起销毁
+      document.getElementById('tableFadi').appendChild(this.videoEle)
+      this.videoEle.addEventListener('play', this.played)
+      this.videoEle.addEventListener('pause', this.paused)
+      this.$once('hook:beforeDestroy', () => {
+        this.videoEle.removeEventListener('play', this.played)
+        this.videoEle.removeEventListener('pause', this.paused)
       })
-    }
+      // 渲染后开始计时
+      this.setTimer()
+    })
   },
   beforeDestroy () {
-    if (this.player) {
-      this.player.stop()
-      this.player = null
-    }
+    // 页面离开暂停播放
+    this.pauseVideo()
   },
   methods: {
-    playAudio () {
+    // 加载计时
+    setTimer () {
+      this.startTime = (new Date()).getTime()
+      this.timer = setInterval(() => {
+        const currtime = (new Date()).getTime()
+        // 比10s大
+        if (currtime - this.startTime >= 10000) {
+          clearInterval(this.timer)
+          this.timer = null
+          // 超过10s没有加载出视频显示提示
+          if (!this.isReady) {
+            Dialog.alert({
+              message: '视频加载失败，请重试',
+              theme: 'round-button',
+              className: 'detail-dialog',
+              confirmButtonText: '重新加载',
+              confirmButtonColor: '#34B7B9'
+            }).then(() => {
+              // 视频重新渲染
+              this.videoEle.src = this.url
+              this.setTimer()
+              console.log('重新加载视频')
+            })
+          }
+        }
+      }, 1000)
+      // 中途离开避免继续计时触发
+      this.$once('hook:beforeDestroy', () => {
+        if (this.timer) clearInterval(this.timer)
+      })
+    },
+    // 开始播放
+    played () {
+      console.log('播放')
+      if (this.timer) {
+        clearInterval(this.timer)
+        this.timer = null
+      }
+      this.isReady = true
+      this.isPlay = true
+    },
+    // 视频暂停（自动播放结束和手动暂停）
+    paused () {
+      this.isPlay = false
+      this.$emit('playVideo', true)
+      this.videoEle.removeEventListener('play', this.played)
+      this.videoEle.removeEventListener('pause', this.paused)
+    },
+    // 手动暂停
+    pauseVideo () {
       if (!this.isPlay) return
-      if (this.player) this.player.stop()
+      // 暂停并将视频归位
+      this.videoEle.pause()
+      this.videoEle.currentTime = 0
     }
   }
 }
 </script>
+<style>
+.dialog-box-left-video {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  transform: scaleX(1);
+}
+</style>
 <style lang="less" scoped>
 .main {
   display: flex;
-  align-items: center;
+  align-items: flex-end;
+  padding: 10px 0;
 }
 .boxLeft {
   min-width: 40px;
   max-width: 70%;
-  min-height: 30px;
-  padding: 6px 14px;
+  min-height: 20px;
+  padding: 14px 14px 14px 10px;
   display: inline-flex;
   align-items: center;
   position: relative;
-  background: #FFFFFF;
+  background: rgba(255, 255, 255, .8);
   border-radius: 8px;
-  margin: 10px 0;
+  // margin: 10px 0;
   margin-left: 26px;
   font-size: 14px;
-  color: #555555;
+  color: #000;
 }
 .boxLeft:after {
   content: '';
@@ -106,5 +189,17 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  opacity: .8;
+  &.run {
+    animation: run .5s infinite;
+  }
+}
+@keyframes run {
+  0% {
+    transform: rotateZ(360deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
 }
 </style>
