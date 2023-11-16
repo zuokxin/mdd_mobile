607e04a7c2cdc91e6e9d6c5a3fd20115b0efe423
linmiaomiao <linmiaomiao@fanhantech.com>
2023-11-16 02:33:00 +0000

    安卓模板和iOS关闭资源

--- src/views/test-do/test-do-fadi-gbt/index.vue
+++ src/views/test-do/test-do-fadi-gbt/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <FadiIos/>
+    <FadiIos v-if="browserType === 'isIOS'"/>
+    <FadiAndroid v-else/>
   </div>
 </template>
 <script>
+import browser from '@/utils/browser'
 import FadiIos from './fadi-ios.vue'
+import FadiAndroid from './fadi-android.vue'
 document.addEventListener('visibilitychange', function () {
   if (document.visibilityState === 'hidden') {
     // window.location.reload()
@@ -15,15 +18,19 @@ document.addEventListener('visibilitychange', function () {
   }
 })
 export default {
-  name: 'fadi-gbt',
+  name: 'fadi-gbt-index',
   components: {
-    FadiIos
+    FadiIos,
+    FadiAndroid
   },
   data () {
     return {
+      browserType: ''
     }
   },
   created () {
+    if (browser().ios) this.browserType = 'isIOS'
+    console.log(this.browserType)
   },
   beforeRouteLeave (to, from, next) {
     // 离开后摄像头红点消失
